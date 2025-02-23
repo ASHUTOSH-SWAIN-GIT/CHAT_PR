6d68de30cf09c3c8f2d495071136bde39e012a1e
ASHUTOSH-SWAIN-GIT <ashutoshswain7383@gmail.com>
2025-02-24 00:38:23 +0530

    fixed dockerfile

--- backend/server.js
+++ backend/server.js
@@ -35,7 +35,7 @@ mongoose
   })
   .then(() => {
     console.log("✅ Connected to MongoDB");
-    server.listen(port, () => {
+    server.listen(port,`0.0.0.0`, () => {
       console.log(`🚀 Server is running on port ${port}`);
     });
   })
